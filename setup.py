b050ea74048b26b7276495fe1de4a81988480bf6
nhfruchter <nhfruchter@cmu-840944.wv.cc.cmu.edu>
2014-09-16 21:10:22 -0400

    pypi version bump

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 setup(
     name="pgh-bustime",
-    version='0.1.1',
+    version='0.1.2',
     author='Nathaniel Fruchter',
     author_email='fruchter@cmu.edu',
     packages=['pghbustime'],
     url='http://github.com/nhfruchter/pgh-bustime',
     license='LICENSE',
     description='Python wrapper for the Port Authority of Allegheny County realtime bus information API.',
-    install_requires=['xmltodict', 'requests', 'repoze.lru']
+    install_requires=['xmltodict', 'requests', 'repoze.lru', 'pytz']
 )
